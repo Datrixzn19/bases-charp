91748ab43897efb6c3606135ac5a9546ebcfaa10
David19 <davidleonel911@gmail.com>
2026-04-24 20:17:12 -0500

    COMM

--- Program.cs
+++ Program.cs
@@ -8,17 +8,17 @@ int.TryParse(Console.ReadLine(), out int n_pedidos);
 int n_vueltas = 0;
 while(n_vueltas < n_pedidos)
 { 
-    // IMPORTANTÍSIMO: Crear el objeto DENTRO del bucle
+    // Crear el obj dentro del bucle 
     Pedido pedido = new Pedido();
 
-    Console.WriteLine($"\n--- Pedido #{n_vueltas + 1} ---");
-    
+    Console.WriteLine($"\nPedido #{n_vueltas + 1}");
+
+        //NOMBRE
     Console.WriteLine("Nombre del plato: ");
     pedido.NombrePlato = Console.ReadLine();
 
+        //PRECIO
     Console.WriteLine("Precio del plato: ");
-    // Nota: TryParse no lanza excepciones, así que el try-catch aquí no haría falta,
-    // pero si usas decimal.Parse() sí es necesario.
     if (!decimal.TryParse(Console.ReadLine(), out decimal precio))
     {
         Console.WriteLine("Formato no válido, se asigna 10.00");
@@ -26,13 +26,12 @@ while(n_vueltas < n_pedidos)
     }
     pedido.Precio = precio;
 
+        //CATEGORIA
     Console.WriteLine("Categoría (0-Entrada, 1-Principal, 2-Postre): ");
     int.TryParse(Console.ReadLine(), out int cat);
     
-    // Aquí la parte que te faltaba: Asignación con Casting
-    pedido.Categoria = (TipoPlato)cat;
 
-    // guardar en la lista
+    pedido.Categoria = (TipoPlato)cat;
     pedidos.Add(pedido);
 
     n_vueltas++; 
